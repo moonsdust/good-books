4eb9d735ea5448bbf4eb6487b815260b1e8a231e
Emily Su <emilyzqsu@gmail.com>
2024-11-16 21:30:27 -0500

    Added install packages script

---
+++ scripts/00-install_packages.R
@@ -0,0 +1,17 @@
+#### Preamble ####
+# Purpose: Installs packages needed to run scripts and Quarto document
+# Author: Emily Su
+# Date: 16 November 2024
+# Contact: em.su@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: -
+# NOTE: This script was checked through lintr for styling
+
+#### Workspace setup ####
+## Installing packages (only needs to be done once per computer)
+install.packages("tidyverse") # Contains data-related packages
+install.packages("janitor") # To clean datasets
+install.packages("dplyr")
+install.packages("ggplot2") # To make graphs
+install.packages("lintr") # To check styling of code
+install.packages("styler") # To style code
