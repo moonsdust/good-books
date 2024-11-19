d78e1a09e3afc0c270c97420a6fffb9b851f82b0
Emily Su <emilyzqsu@gmail.com>
2024-11-19 16:22:51 -0500

    Finished download data script and obtained raw data

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,26 +1,29 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from the GitHub repo owned by 
+# The Washington Post containing data of criminal homicides in 50 of the 
+# largest US cities from 2010 to 2017.
+# Author: Emily Su
+# Date: 16 November 2024
+# Contact: em.su@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Have ran 00-install_packages.R beforehand to install the
+# necessary packages.
+# NOTE: This script was checked through lintr for styling
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Dataset (Unsolved Homicide Database):
+# https://github.com/washingtonpost/data-homicides
 
-
+# 1. Obtain The Washington Posts's Unsolved Homicide Dataset from GitHub
+data_homicides_dataset <- 
+  read_csv("https://raw.githubusercontent.com/washingtonpost/data-homicides/refs/heads/master/homicide-data.csv", show_col_types = FALSE)
+# NOTE: If the name of the file changes or you are unable to read in the
+# CSV file, please head to https://github.com/washingtonpost/data-homicides
+# for the new name of the CSV file. 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+write_csv(data_homicides_dataset, "data/01-raw_data/raw_data_homicides.csv") 
